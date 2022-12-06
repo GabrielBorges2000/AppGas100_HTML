b71208fc0ec954ef0fd93e0e98a3c5e6230e83d1
Gabriel_Borges <gabriel.borgesoliveira.7@hotmail.com>
2022-12-06 14:52:15 -0300

    Gabriel

--- server.js
+++ server.js
@@ -2,29 +2,40 @@ const express = require('express');
 const app = express();
 
 app.get("/", function (req, res){
-    res.sendFile(__dirname + "/src/pages/Home/index.html");
+    res.sendFile(__dirname + "/src/Home.html");
+});
+
+app.get("/Header", function (req, res){
+    res.sendFile(__dirname + "/src/Header.html");
+});
+
+app.get("/Card", function (req, res){
+    res.sendFile(__dirname + "/src/Card.html");
 });
 
 app.get("/cadastro", function (req, res){
-    res.sendFile(__dirname + "/src/pages/Cadastro/index.html");
+    res.sendFile(__dirname + "/src/Cadastro.html");
 });
 
 app.get("/cadastro/name", function (req, res){
-    res.sendFile(__dirname + "/src/pages/CadastroName/index.html");
+    res.sendFile(__dirname + "/src/CadastroName.html");
 });
 
-app.get("/pagamento/compra", function (req, res){
-    res.sendFile(__dirname + "/src/pages/pagamento/compra/index.html");
+app.get("/Compra", function (req, res){
+    res.sendFile(__dirname + "/src/compra.html");
 });
 
-app.get("/pagamento/pagemento", function (req, res){
-    res.sendFile(__dirname + "/src/pages/pagemento/pagamento/index.html");
+app.get("Pagemento", function (req, res){
+    res.sendFile(__dirname + "/src/Pagemento.html");
 });
 
-app.get("/pagamento/confirmacao", function (req, res){
-    res.sendFile(__dirname + "/src/pages/pagemento/confirmacao/index.html");
+app.get("/Confirmacao", function (req, res){
+    res.sendFile(__dirname + "/src/Confirmacao.html");
 });
 
 app.listen(3000);
-console.log("Rodando Servidor!");
+console.log(
+    "Rodando Servidor!",
+    "Acesse: http://localhost:3000/",
+    );
 
